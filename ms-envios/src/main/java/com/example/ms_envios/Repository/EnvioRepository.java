4f71b658e104470b14b67cb2c05c66f78d114343
Ax-Lv <axel.levin.seron@gmail.com>
2026-06-15 13:26:02 -0400

    refactor: eliminar @Repository innecesario en EnvioRepository

--- ms-envios/src/main/java/com/example/ms_envios/Repository/EnvioRepository.java
+++ ms-envios/src/main/java/com/example/ms_envios/Repository/EnvioRepository.java
@@ -2,10 +2,9 @@ package com.example.ms_envios.Repository;
 
 
 
-import com.example.ms_envios.Model.Envio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import com.example.ms_envios.Model.Envio;
+
 public interface EnvioRepository extends JpaRepository<Envio, Long> {
 }
