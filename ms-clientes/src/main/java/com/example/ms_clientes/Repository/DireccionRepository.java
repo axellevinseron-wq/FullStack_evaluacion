122ac49d499c99e6c3710893da46a1c129037224
Ax-Lv <axel.levin.seron@gmail.com>
2026-06-15 13:23:51 -0400

    refactor: eliminar @Repository innecesario en DireccionRepository

--- ms-clientes/src/main/java/com/example/ms_clientes/Repository/DireccionRepository.java
+++ ms-clientes/src/main/java/com/example/ms_clientes/Repository/DireccionRepository.java
@@ -3,8 +3,6 @@ package com.example.ms_clientes.Repository;
 
 import com.example.ms_clientes.Model.Direccion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DireccionRepository extends JpaRepository<Direccion, Long> {
 }
