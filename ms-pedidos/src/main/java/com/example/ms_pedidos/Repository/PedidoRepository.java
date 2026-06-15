365978249073037c14d82c159420e09d8c73fc19
Ax-Lv <axel.levin.seron@gmail.com>
2026-06-15 11:35:46 -0400

    Mejora de sintaxis

--- ms-pedidos/src/main/java/com/example/ms_pedidos/Repository/PedidoRepository.java
+++ ms-pedidos/src/main/java/com/example/ms_pedidos/Repository/PedidoRepository.java
@@ -1,10 +1,9 @@
 package com.example.ms_pedidos.Repository;
 
 
-import com.example.ms_pedidos.Model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import com.example.ms_pedidos.Model.Pedido;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
