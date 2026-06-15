09e614ba355b3b84202060ec23e1612614694fd9
Ax-Lv <axel.levin.seron@gmail.com>
2026-06-15 13:06:47 -0400

    refactor: eliminar @Repository innecesario en MensajeRepository

--- ms_notificaciones/src/main/java/com/example/ms_notificaciones/Repository/MensajeRepository.java
+++ ms_notificaciones/src/main/java/com/example/ms_notificaciones/Repository/MensajeRepository.java
@@ -2,10 +2,9 @@ package com.example.ms_notificaciones.Repository;
 
 
 
-import com.example.ms_notificaciones.Model.Mensaje;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import com.example.ms_notificaciones.Model.Mensaje;
+
 public interface MensajeRepository extends JpaRepository<Mensaje, Long> {
 }
