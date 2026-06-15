30d4f9ffdd4cc2e4e8409fce9841e2179e16996e
Ax-Lv <axel.levin.seron@gmail.com>
2026-06-15 13:18:44 -0400

    refactor: eliminar @Repository innecesario en ClienteRepository

--- ms-clientes/src/main/java/com/example/ms_clientes/Repository/ClienteRepository.java
+++ ms-clientes/src/main/java/com/example/ms_clientes/Repository/ClienteRepository.java
@@ -3,8 +3,6 @@ package com.example.ms_clientes.Repository;
 
 import com.example.ms_clientes.Model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
